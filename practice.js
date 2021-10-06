e90b671cf72f04ccaca435c68daa9da18548bc01
Nattachai Nudol <nattachainudol@gmail.com>
2021-10-06 10:58:24 -0700

    Completed _.filter

--- practice.js
+++ practice.js
@@ -1,6 +1,6 @@
 // This repo is optional extra practice to use the underscore functions.
-// Here we'll be writing new functions, but these functions will use 
-// the underscore functions within them. 
+// Here we'll be writing new functions, but these functions will use
+// the underscore functions within them.
 
 /*
  *
@@ -22,7 +22,9 @@ var moreFruits = function(fruits) {
 // use _.each to traverse the number array and determine
 // which are multiples of five.
 var multiplesOfFive = function(numbers) {
-
+  return _.each(numbers, function (element) {
+    return element;
+  });
 };
 
 /*
@@ -33,18 +35,25 @@ var multiplesOfFive = function(numbers) {
 
 // use _.filter to return the fruits array with only the desired fruit.
 var onlyOneFruit = function(fruits, targetFruit) {
-
+  return _.filter(fruits, function(eachFruit) {
+    return eachFruit === targetFruit;
+  });
 };
 
 // use _.filter to return the fruits array with only fruits
 // starting with the letter 'P'.
 var startsWith = function(fruits, letter) {
-
+  return _.filter(fruits, function(eachFruit) {
+    var fruitFirstLetter = eachFruit[0];
+    return fruitFirstLetter === letter;
+  });
 };
 
 // return a filtered array containing only cookie-type desserts.
 var cookiesOnly = function(desserts) {
-
+  return _.filter(desserts, function(dessert) {
+    return desserts.type === 'cookie';
+  });
 };
 
 /*
@@ -55,7 +64,10 @@ var cookiesOnly = function(desserts) {
 
 // return the total price of all products.
 var sumTotal = function(products) {
-  
+  var totalPrice = _.reduce(products, function(sum, price) {
+    return sum += price;
+  });
+  return totalPrice;
 };
 
 // return an object consisting of dessert types and how many of each.
@@ -68,7 +80,7 @@ var dessertCategories = function(desserts) {
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
 var ninetiesKid = function(movies) {
-  
+
 };
 
 // return an boolean stating if there exists a movie with a shorter
@@ -98,7 +110,7 @@ var movieNight = function(movies, timeLimit) {
   ];
 */
 var applyCoupon = function(grocery, coupon) {
-    
+
 };
 
 // given an array of strings, use _.map to return a new array containing all
