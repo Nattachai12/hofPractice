92f19823ffc75164ae93a75cf4965ee8cc12440f
Nattachai Nudol <nattachainudol@gmail.com>
2021-10-17 11:58:20 -0700

    Complete _.reduce

--- practice.js
+++ practice.js
@@ -21,10 +21,25 @@ var moreFruits = function(fruits) {
 
 // use _.each to traverse the number array and determine
 // which are multiples of five.
+/*
+  input: number array
+  output: number (the count of elements that are multile of 5)
+
+  strategy
+  1. create count var.
+  1. use _.each to loop through the array
+  2. use if condition for element%5 === 0
+  3. count ++
+  4. return count
+*/
 var multiplesOfFive = function(numbers) {
-  return _.each(numbers, function (element) {
-    return element;
+  var count = 0;
+  _.each(numbers, function (element) {
+    if (element % 5 === 0) {
+      count++;
+    }
   });
+  return count;
 };
 
 /*
@@ -63,31 +78,88 @@ var cookiesOnly = function(desserts) {
  */
 
 // return the total price of all products.
+/*
+input: array of object with price property
+output: sum of prices
+
+strategy
+1. loop into product
+2. create an array of prices
+3. use reduce
+
+*/
 var sumTotal = function(products) {
-  var totalPrice = _.reduce(products, function(sum, price) {
-    return sum += price;
+  var prices = _.map(products, function (product) {
+    return product.price;
   });
-  return totalPrice;
+  return _.reduce(prices, function(sum, price) {
+    return sum + price;
+  }, 0);
 };
 
 // return an object consisting of dessert types and how many of each.
 // exampleOutput: { dessertType: 3, dessertType2: 1 }
-var dessertCategories = function(desserts) {
+/*
+input: array of objects with type property name.
+output: an obj with dessert type name as a key and count how
+many of each type (value)
 
+strategy
+1. use map to get obj.type (string) in an array
+2. use reduce
+*/
+var dessertCategories = function(desserts) {
+  var types = _.map(desserts, function (dessert) {
+    return dessert.type;
+  });
+  return _.reduce(types, function (obj, type) {
+    if (!obj[type]) {
+      obj[type] = 0;
+    }
+    obj[type]++;
+    return obj;
+  }, {});
 };
 
 // given an array of movie data objects,return an array containing
 // movies that came out between 1990 and 2000.
 // TIP: use an array as your accumulator - don't push to an external array!
-var ninetiesKid = function(movies) {
+/*
+input: an obj (movies) array with release year property (number)
+output: an array containing all movie names (movie.title)
+that were release between 1990 -2000
 
+strategy
+1. use reduce
+*/
+var ninetiesKid = function(movies) {
+  return _.reduce(movies, function (uniqueMovie, movie) {
+    if (!uniqueMovie.includes(movie.title) && (movie.releaseYear >= 1990
+      && movie.releaseYear <= 2000)) {
+      uniqueMovie.push(movie.title);
+    }
+    return uniqueMovie;
+  }, []);
 };
 
 // return an boolean stating if there exists a movie with a shorter
 // runtime than your time limit.
 // timeLimit is an integer representing a number of minutes.
-var movieNight = function(movies, timeLimit) {
+/*
+input: movie(obj) array and run time (number)
+output: true if there is a movie with a shorter runtime
+false if vice versa
 
+strategy:
+1. use reduce
+*/
+var movieNight = function(movies, timeLimit) {
+  return _.reduce(movies, function(shortestTime, movie) {
+    if (shortestTime) {
+      return true;
+    }
+    return movie.runtime < timeLimit;
+  }, false);
 };
 
 /*
